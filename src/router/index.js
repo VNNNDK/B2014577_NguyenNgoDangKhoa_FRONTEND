f3758d3a966a6596b05998048fa5ba0bd96a8b1c
VNNNDK <khoab2014577@student.ctu.edu.vn>
2023-10-26 07:16:50 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:patchMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 
 const router = createRouter({
